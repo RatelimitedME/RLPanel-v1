5ead7817aeeda2b7449f1752188d0ec3958b3194
MucaP <mucapapipa.br@gmail.com>
2018-01-18 16:47:26 -0200

    Dark mode nibba

---
+++ panel/js/core/dark_mode.js
@@ -0,0 +1,13 @@
+function activateDark() {
+    $("body").animate({"background-color": "#212121"}, 300);
+    $(".card").animate({"background-color": "#161616"}, 300);
+    $("p, h3, h4, span, label").animate({"color": "#fafafa"}, 300);
+    $('#toggle').attr('onClick','activateLight()');
+}
+
+function activateLight() {
+    $("body").removeAttr("style");
+    $(".card").removeAttr("style")
+    $("p, h3, h4, span, label").removeAttr("style")
+    $('#toggle').attr('onClick','activateDark()');
+}
