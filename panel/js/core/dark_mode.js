a09d93a6077943aa7282e56d9bb9f5b40ed2d2f9
MucaP <mucapapipa.br@gmail.com>
2018-01-19 15:36:25 -0200

    Cookies

--- panel/js/core/dark_mode.js
+++ panel/js/core/dark_mode.js
@@ -1,11 +1,14 @@
 function activateDark() {
+    document.cookie = "dark=true";
     $("body").animate({"background-color": "#212121"}, 300);
     $(".card").animate({"background-color": "#161616"}, 300);
     $("p, h3, h4, span, label").animate({"color": "#fafafa"}, 300);
     $('#toggle').attr('onClick','activateLight()');
+    
 }
 
 function activateLight() {
+    document.cookie = "dark=false";
     $("body").removeAttr("style");
     $(".card").removeAttr("style")
     $("p, h3, h4, span, label").removeAttr("style")
