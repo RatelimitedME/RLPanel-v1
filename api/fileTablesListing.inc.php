be5f6cae7148814899ddddc8c056f39fcd679a2a
root <root@danielxd.us>
2018-03-13 09:22:51 -0500

    Remove pure PHP table, add API to retreive user uploads and use that with AJAX instead of PHP and foreach

--- api/fileTablesListing.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-include __DIR__ . '/../../pgdbcreds.inc.php';
-
-/* Postgres queries to array all files posted by the user */
-$getAllFilesByUserQuery = "SELECT * FROM logs WHERE token='" . $_SESSION['userToken'] . "'"; 
-$getAllFilesByUserExecution = pg_exec($database, $getAllFilesByUserQuery);
-$getAllFilesByUserRows = pg_fetch_all($getAllFilesByUserExecution);
-?>
